79da517d95182bb9db6695de6bd4eb35a7927b82
Promise <49518212+PromiseWang@users.noreply.github.com>
2021-11-17 14:14:20 +0800

    Update Setting.h

--- Setting.h
+++ Setting.h
@@ -10,7 +10,7 @@ using namespace std;
 #define HIDDEN_NODE 100
 #define OUTPUT_NODE 10
 #define RATE_SIGMOID 0.1
-#define RATE_TANH 0.001
+#define RATE_TANH 0.0001
 #define RATE_RELU 0.0001
 
 #endif //BP2_SETTING_H
